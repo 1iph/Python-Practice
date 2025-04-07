42f0e9aad6a61598ecc31cb4a7e90dadf5b897ef
1iph <nmcclung9@gmail.com>
2025-04-07 08:27:16 +0000

    1 - Step Calculator

--- StepCounter.py
+++ StepCounter.py
@@ -1,1 +1,11 @@
+print("STEP COUNTER")
+goal = int (input("What is your step goal today? "))
+total_steps = int (input("How many steps are you at currently? "))
 
+remaining = goal - total_steps
+if remaining > 0:
+    print(f"You need {remaining} more steps to reach your goal!")
+if remaining < 0:
+    print(f"Congratulations! You've exceeded your goal by {-remaining} steps!")  
+if remaining == 0:
+    print("Congratulations! You've reached your goal!")
