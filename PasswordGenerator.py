5eed3ad922ecbc6aa085ea1e55074c9458223813
1iph <nmcclung9@gmail.com>
2025-05-16 14:08:48 +0000

    19-PasswordGenerator.py

---
+++ PasswordGenerator.py
@@ -0,0 +1,24 @@
+import random
+import string
+
+length = int(input("How long would you like this password to be?: "))
+how_many = int(input("How many passwords would you like to generate?: "))
+safe_symbol = "!"
+
+for _ in range(how_many):
+ all_possible_chars = string.ascii_letters + string.digits + safe_symbol
+
+
+ generated_password = ""
+
+
+ for _ in range(length):
+    
+    random_chosen_char = random.choice(all_possible_chars)
+
+    
+    generated_password += random_chosen_char
+
+
+ print("Your generated password is:", generated_password)
+ print("Keep it in a safe placce!")
